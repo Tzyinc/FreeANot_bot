0a7cbbda4bdaabae4bc9c51a542dcd85bfb71c41
Tzyinc <tzyinc@hotmail.com>
2017-08-16 16:19:32 +0800

    Added sqlite example

---
+++ sqlLiteModule.js
@@ -0,0 +1,25 @@
+var sqlite3 = require('sqlite3').verbose()
+var db = new sqlite3.Database('testdb.tb')
+
+db.serialize(function () {
+  db.run('DROP TABLE user')
+  db.run('CREATE TABLE user ' +
+    '(userId     INT            NOT NULL,' +
+    'firstname   VARCHAR(255)   NOT NULL,' +
+    'timetable   NCHAR(40)      NOT NULL,' +
+    'username    VARCHAR(255)' +
+    ')')
+
+  var stmt = db.prepare('INSERT INTO user (userId, firstname, timetable, username) VALUES (0, "ten", "1111111111111111111111111111111111111111", "tenten")')
+  stmt.run()
+  stmt.finalize()
+
+  db.each('SELECT * FROM user', function (err, row) {
+    console.log(row)
+    if (err) {
+      console.log(err)
+    }
+  })
+})
+
+db.close()
