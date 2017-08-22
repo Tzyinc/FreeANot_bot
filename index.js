d83434fe0b08f018dbfda751604ea613b467d86b
Tzyinc <tzyinc@hotmail.com>
2017-08-22 21:26:16 +0800

    Attempt to fix deployment bug

--- index.js
+++ index.js
@@ -415,7 +415,7 @@ function isEvenWeek (today) {
 function getCurrentSlot (time) {
   var day = time.getDay() - 1
   var timeStr = ('0' + time.getHours()).slice(-2) + '00'
-
+  console.log(timeStr)
   var timeArray = ['0800', '0900', '1000', '1100', '1200', '1300', '1400', '1500', '1600', '1700', '1800', '1900', '2000', '2100', '2200', '2300', '0000']
   var slot = -1
   for (var i = 0; i < timeArray.length; i++) {
