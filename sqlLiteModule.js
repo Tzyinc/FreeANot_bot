b09893a4a2d1a93aa7295730a56cd8e3ffcbf783
Tzyinc <tzyinc@hotmail.com>
2017-08-22 18:27:15 +0800

    changed all functions in sqlite module to return promises

--- sqlLiteModule.js
+++ sqlLiteModule.js
@@ -3,18 +3,19 @@ var db = new sqlite3.Database('freeAnot.tb')
 const constraintErr = 'SQLITE_CONSTRAINT'
 
 function getAllDataFromUsers () {
-  db.all('SELECT * FROM user', function (err, row) {
-    if (err) {
-      console.error(err)
-      return err
-    }
-    console.log(row)
-    return row
+  var p = new Promise(function (resolve, reject) {
+    db.all('SELECT * FROM user', function (err, row) {
+      if (err) {
+        reject(err)
+      }
+      resolve(row)
+    })
   })
+  return p
 }
 
 function getAllUsersInGroup (groupId) {
-  var userPromise = new Promise(function (resolve, reject) {
+  var p = new Promise(function (resolve, reject) {
     db.all('SELECT * ' +
     'FROM user ' +
     'WHERE EXISTS ' +
@@ -22,94 +23,112 @@ function getAllUsersInGroup (groupId) {
       $groupId: groupId
     }, function (err, row) {
       if (err) {
-        console.error(err)
         reject(err)
       }
-      console.log(row)
       resolve(row)
     })
   })
-  return userPromise
+  return p
 }
 
 function addUserToChat (userId, chatId) {
-  db.run('INSERT INTO userGroupRelations VALUES($userId, $groupId)', {
-    $userId: userId,
-    $groupId: chatId
-  }, function (err) {
-    if (err) {
-      console.error(err)
-    }
+  var p = new Promise(function (resolve, reject) {
+    db.run('INSERT INTO userGroupRelations VALUES($userId, $groupId)', {
+      $userId: userId,
+      $groupId: chatId
+    }, function (err) {
+      if (err) {
+        reject(err)
+      }
+      resolve()
+    })
   })
+  return p
 }
 
 function insertStudentToUsers (id, fName, oTime, eTime, uName) {
-  if (uName) {
-    db.run('INSERT INTO user VALUES($userId, $firstname, $oddtimetable, $eventimetable, $username)', {
-      $userId: id,
-      $firstname: fName,
-      $oddtimetable: oTime,
-      $eventimetable: eTime,
-      $username: uName
-    }, function (err) {
-      if (err) {
-        if (err.code === constraintErr) {
-          updateStudentToUsers(id, fName, oTime, eTime, uName)
-        } else {
-          console.error(err)
+  var p = new Promise(function (resolve, reject) {
+    if (uName) {
+      db.run('INSERT INTO user VALUES($userId, $firstname, $oddtimetable, $eventimetable, $username)', {
+        $userId: id,
+        $firstname: fName,
+        $oddtimetable: oTime,
+        $eventimetable: eTime,
+        $username: uName
+      }, function (err) {
+        if (err) {
+          if (err.code === constraintErr) {
+            updateStudentToUsers(id, fName, oTime, eTime, uName).then(
+              resolve()
+            )
+          } else {
+            reject(err)
+          }
         }
-      }
-    })
-  } else {
-    db.run('INSERT INTO user (userId, firstName, oddtimetable, eventimetable)' +
-    ' VALUES($userId, $firstname, $oddtimetable, $eventimetable)', {
-      $userId: id,
-      $firstname: fName,
-      $oddtimetable: oTime,
-      $eventimetable: eTime,
-      $username: uName
-    }, function (err) {
-      if (err) {
-        if (err.code === constraintErr) {
-          updateStudentToUsers(id, fName, oTime, eTime, uName)
-        } else {
-          console.error(err)
+      })
+    } else {
+      db.run('INSERT INTO user (userId, firstName, oddtimetable, eventimetable)' +
+      ' VALUES($userId, $firstname, $oddtimetable, $eventimetable)', {
+        $userId: id,
+        $firstname: fName,
+        $oddtimetable: oTime,
+        $eventimetable: eTime,
+        $username: uName
+      }, function (err) {
+        if (err) {
+          if (err.code === constraintErr) {
+            updateStudentToUsers(id, fName, oTime, eTime, uName).then(
+              resolve()
+            )
+          } else {
+            reject(err)
+          }
         }
-      }
-    })
-  }
+      })
+    }
+  })
+  return p
 }
 
 function updateStudentToUsers (id, fName, oTime, eTime, uName) {
-  if (uName) {
-    db.run('UPDATE user SET ' +
-    'firstname = $firstname, ' +
-    'oddtimetable = $oddtimetable, ' +
-    'eventimetable = $eventimetable, ' +
-    'username = $username ' +
-    'WHERE userId = $userId', {
-      $userId: id,
-      $firstname: fName,
-      $oddtimetable: oTime,
-      $eventimetable: eTime,
-      $username: uName
-    }, function (err) {
-      console.error(err)
-    })
-  } else {
-    db.run('UPDATE user SET ' +
-    'firstname = $firstname, ' +
-    'oddtimetable = $oddtimetable, ' +
-    'eventimetable = $eventimetable ' +
-    'WHERE userId = $userId', {
-      $userId: id,
-      $firstname: fName,
-      $oddtimetable: oTime,
-      $eventimetable: eTime
-    }, function (err) {
-      console.error(err)
-    })
-  }
+  var p = new Promise(function (resolve, reject) {
+    if (uName) {
+      db.run('UPDATE user SET ' +
+      'firstname = $firstname, ' +
+      'oddtimetable = $oddtimetable, ' +
+      'eventimetable = $eventimetable, ' +
+      'username = $username ' +
+      'WHERE userId = $userId', {
+        $userId: id,
+        $firstname: fName,
+        $oddtimetable: oTime,
+        $eventimetable: eTime,
+        $username: uName
+      }, function (err) {
+        if (err) {
+          reject(err)
+        }
+        resolve()
+      })
+    } else {
+      db.run('UPDATE user SET ' +
+      'firstname = $firstname, ' +
+      'oddtimetable = $oddtimetable, ' +
+      'eventimetable = $eventimetable ' +
+      'WHERE userId = $userId', {
+        $userId: id,
+        $firstname: fName,
+        $oddtimetable: oTime,
+        $eventimetable: eTime
+      }, function (err) {
+        if (err) {
+          reject(err)
+        }
+        resolve()
+      })
+    }
+  })
+  return p
 }
 
 module.exports = {
